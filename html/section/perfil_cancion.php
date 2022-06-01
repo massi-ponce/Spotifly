9e2ba6df98ef27517f42aa242e956106068eb90a
Sealra <71726988+Sealra@users.noreply.github.com>
2022-05-31 21:55:59 -0400

    tabla canciones

---
+++ html/section/perfil_cancion.php
@@ -0,0 +1,37 @@
+<?php
+    require_once("../validar.php");
+?>
+
+<?php include('../template/header.html');?>
+
+<?php
+    if(isset($_GET["nombre_cancion"]) && isset($_GET["nombre_artista"]) && isset($_GET["fecha_composicion"]) && isset($_GET["album"]) && isset($_GET["letra"]))
+    {
+        $nombre_cancion = $_GET["nombre_cancion"];
+        $nombre_artista = $_GET["nombre_artista"];
+        $fecha_composicion = $_GET["fecha_composicion"];
+        $album = $_GET["album"];
+        $letra = $_GET["letra"];
+    }
+?>
+
+<DOCTYPE html>
+<html>
+    <body> 
+        <div class="container">
+            <div class="col-sm-6" style="padding-top:50px;">
+                <div class="well">
+                    <h1><?php echo $nombre_cancion ?></h1>
+                    <h4><?php echo $nombre_artista ?></h4>
+                    <h5><?php echo $album ?> </h5>
+                    <h6><?php echo $fecha_composicion?></h6>
+                </div>
+                <h2><?php echo "Letra:"?></h2>
+                <p> <?php echo $letra ?></p>
+            </div>
+        </div>
+
+    </body>
+</html>
+</DOCTYPE>
+
---
+++ html/section/tabla_canciones.php
@@ -0,0 +1,46 @@
+<?php
+    require_once("../validar.php");
+?>
+<?php include('../template/header.html');?>
+
+<?php 
+    include('response.php');
+    $newObj = new cancion();
+    $songs = $newObj->getCancion();
+?>
+
+<DOCTYPE html>   
+<html>
+    <body>
+        <div class="container">
+          <div class="col-sm-6" style="padding-top:50px;">
+              <div class="well">
+                  <h2>Tabla de Canciones</h2>
+              </div>
+              <table id="songs_grid" class="table" width="100%" cellspacing="0">
+                  <thead>
+                      <tr>
+                          <th>Nombre</th>
+                          <th>Artista(s)</th>
+                          <th>Álbum</th>
+                          
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <?php foreach($songs as $key => $song) :?>
+                      <tr>
+                          <td><?php echo $song['nombre'] ?></td>
+                          <td><?php echo $song['nombre_artistico'] ?></td>
+                          <td><?php echo $song['nombre_album'] ?></td>
+                          <td><button onclick="location.href='perfil_cancion.php?nombre_cancion=<?php echo $song['nombre']?>&nombre_artista=<?php echo $song['nombre_artistico']?>&fecha_composicion=<?php echo $song['fecha_composicion']?>&album=<?php echo $song['nombre_album']?>&letra=<?php echo $song['letra']?>'">Perfil</button></td>
+                      </tr>
+                      
+                  <?php endforeach;?>
+                  </tbody>
+              </table>
+          </div>
+        </div>
+      </div>
+    </body>
+</html>
+</DOCTYPE>
