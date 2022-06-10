291a21a3f7f2125635b0253d7f702fadf837ef80
unknown <vperezc18@gmail.com>
2022-06-10 17:50:26 -0400

    arreglando insert canciones

--- html/php/insertar_cancion.php
+++ html/php/insertar_cancion.php
@@ -1,31 +1,34 @@
 <?php
-    require('../../db_config.php');
+require('../../db_config.php');
+session_start();
 
-    $id_album=$_POST["id_album"];
-    $nombre=$_POST["nombre"];
-    $letra=$_POST["letra"];
-    $fecha_composicion=$_POST["fecha_composicion"];
+if(isset($_POST['agregar'])){
+    $id_album = $_POST['id_album'];
+    $nombre=$_POST['nombre'];
+    $letra=$_POST['letra'];
+    $fecha_composicion=$_POST['fecha_composicion'];
+    $query= "INSERT INTO CANCIONES(nombre, letra, fecha_composicion) 
+    VALUES('$nombre','$letra', '$fecha_composicion') RETURNING id_cancion";
+    $consulta = pg_query($dbconn, $query);
+    $row = pg_fetch_row($consulta);
+    $id_cancion = $row[0];
+    $id_artista = $_SESSION['id_artista'];
 
-    $sql_statement1 = "SELECT * from album WHERE id_album = '$id_album';";
-    $result1 = pg_query($dbconn, $sql_statement1);
-
-    $sql_statement2 = "INSERT INTO canciones(nombre, letra, fecha_composicion) VALUES('$nombre', '$letra', '$fecha_composicion');";
-    
-    if(pg_num_rows($result1) == 1){
-        if($result2 = pg_query($dbconn, $sql_statement2)){
-            $sql_statement3 = "SELECT id_cancion FROM canciones WHERE nombre = '$nombre';";
-            $result3 = pg_query($dbconn, $sql_statement3);
-            $variable = pg_fetch_all($result3);
-            foreach($variable as $clave => $valor){
-            $id_cancion = $valor['id_cancion'];
-            }
-            $sql_statement4 = "INSERT INTO album_tiene_cancion(id_album, id_cancion) VALUES('$id_album', '$id_cancion');";
-            if($result4 = pg_query($dbconn, $sql_statement4)){
-                echo "<script> alert('Canción creada correctamente');window.location='../section/crud_canciones.html'</script>"; 
-            }
-        }
+}
+if($consulta){
+    $query2 = "INSERT INTO artista_compuso_cancion(id_artista, id_cancion)
+    VALUES( $id_artista , $id_cancion)";
+    $consulta2 = pg_query($dbconn, $query2);
+    if($consulta2){
+        $query3 = "INSERT INTO album_tiene_cancion(id_album, id_cancion)
+        VALUES($id_album,$id_cancion)";
+        $consulta3 = pg_query($dbconn, $query3);
+        echo "<script> alert('Canción creada correctamente');window.location='../section/crud_canciones.html'</script>"; 
     }
-    else{
-        echo "<script> alert('ERROR: Problemas al crear la canción');window.location='../section/crud_canciones.html'</script>";
-    }
+}else {
+    echo "<script> alert('ERROR: Problemas al crear la canción');window.location='../section/crud_canciones.html'</script>";
+}
+
+
+
 ?>
