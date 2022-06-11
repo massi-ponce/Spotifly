1a0066abdf30210afed1f332b487ea91039e10a6
unknown <vperezc18@gmail.com>
2022-06-10 22:34:19 -0400

    arreglado error eliminar canciones

--- html/php/borrar_cancion.php
+++ html/php/borrar_cancion.php
@@ -1,17 +1,21 @@
 <?php
     require('../../db_config.php');
 
-    $id_cancion=$_GET['id'];
 
-    $sql_statement1 = "DELETE FROM album_tiene_cancion WHERE id_cancion = '$id_cancion';";
+    $id_cancion=$_GET['id_cancion'];
+
+    $sql_statement1 = "DELETE FROM album_tiene_cancion WHERE id_cancion = $id_cancion";
     $result1 = pg_query($dbconn, $sql_statement1);
 
+
     if($result1){
-        echo("WENA");
-        /*$sql_statement2 = "DELETE FROM canciones WHERE id_cancion = '$id_cancion';";
-        if($result2 = pg_query($dbconn, $sql_statement2)){
+        $sql_statement2 = "DELETE FROM artista_compuso_cancion WHERE id_cancion = $id_cancion";
+        $result2 = pg_query($dbconn, $sql_statement2);
+        if($result2){
+            $sql_statement3 = "DELETE FROM canciones WHERE id_cancion = $id_cancion";
+            $result3 = pg_query($dbconn, $sql_statement3);
             header("Location: ../section/crud_canciones.html");
-        }*/
+        }
     }
     else{
         echo "<script> alert('ERROR: No se pudo eliminar la canción');window.location='../section/crud_canciones.html'</script>";
